a97d4cb5d0367f046d51dfd06c0dc6d5eaf11ed7
Tobias Shapinsky <tobiasshapinsky@gmail.com>
2014-01-21 21:23:44 -0800

    basic setup and php, java integration test

---
+++ run.php
@@ -0,0 +1,3 @@
+<?php
+echo shell_exec("java -jar Test bleep");
+?>
---
+++ src/Test.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Owner on 1/21/14.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println(args[0]);
+    }
+}
